f62b9f754bd9df8f53f70f6955dadc16736dec0d
junsoo90 <asdfrf1@daum.net>
2018-05-09 19:16:19 +0900

    txt add

--- lotto/src/Lotto/Lotto_Main.java
+++ lotto/src/Lotto/Lotto_Main.java
@@ -9,6 +9,9 @@ public class Lotto_Main {
 
 	public static void main(String[] args) {
 
+		// a 회사에서 로또번호 추출해줘 - 특정 3개 번호
+		// b 회사에서 로또번호 추출해줘 - 특정 2개 번호
+		
 		Lotto_process lottoprocess = new Lotto_process(); // 로또추출기 생성자
 		lottoprocess.lottoprovide(); // 로또추출기 실행
 
